ca9daee610578f4347f235cde6c5845f526269ae
lauraexplora <74592807+lauraexplora@users.noreply.github.com>
2025-10-07 20:43:20 +0200

    mode3 starfield

--- src/main.c
+++ src/main.c
@@ -5,17 +5,72 @@
 // Example that shows how to play a song with Maxmod and GBT Player at the same
 // time and keep them in sync.
 
+// #include "rand.c"
 #include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <math.h>
 
 #include <gba.h>
+#include <gba_console.h>
 #include <maxmod.h>
 
+#include "toolbox.h"
 #include "gbt_player/gbt_player.h"
 
 #include "soundbank.h"
 #include "soundbank_bin.h"
 
 extern const uint8_t *starlight_brigade_psg[];
+struct Star {
+    int x, y, z, clr;
+    float dir;
+};
+
+#define VCOUNT *(volatile u16*)0x4000006
+volatile s32 RAND_RandomData;
+
+void SeedRandom(void);
+s32 RAND(s32 Value);
+
+// extra stuff, also in tonc_video.h
+#define M3_WIDTH    SCREEN_WIDTH
+// typedef for a whole mode3 line
+typedef COLOR       M3LINE[M3_WIDTH];
+// m3_mem is a matrix; m3_mem[y][x] is pixel (x,y)
+#define m3_mem    ((M3LINE*)MEM_VRAM)
+
+bool clr_flag = false;
+struct Star * create_star(void)
+{
+    struct Star *s = malloc (sizeof (struct Star));
+    s->x = RAND(240);
+    s->y = RAND(160);
+    // s->z = RAND(3);
+
+    // s->clr = clr_flag ? RGB15(205,205,205) : CLR_WHITE;
+    s->clr = CLR_WHITE;
+    clr_flag = !clr_flag;
+
+    s->dir = atan2f(s->y - 79, s->x - 119);
+
+    return s;
+}
+
+
+
+void display_star(struct Star * s)
+{
+    m3_mem[s->y][s->x]= s->clr;
+}
+
+void move_star(struct Star * s, int dist)
+{
+    s->x = s->x + dist * cos(s->dir);
+    s->y = s->y + dist * sin(s->dir);
+}
+
+struct Star * stars[20];
 
 void gbt_sync_to_maxmod(void)
 {
@@ -51,6 +106,17 @@ void gbt_sync_to_maxmod(void)
     // Something went wrong somehow!
 }
 
+/* clear the screen to black */
+void clear_screen(unsigned short color) {
+    unsigned short row, col;
+    /* set each pixel black */
+    for (row = 0; row < SCREEN_HEIGHT; row++) {
+        for (col = 0; col < SCREEN_WIDTH; col++) {
+            m3_mem[row][col]= color;
+        }
+    }
+}
+
 void vbl_handler(void)
 {
     mmVBlank(); // This has to be called exactly at the beginning of VBL
@@ -59,35 +125,63 @@ void vbl_handler(void)
     mmFrame();
     gbt_sync_to_maxmod();
 
+    clear_screen(CLR_BLACK);
+
+    int i;
+    for (i = 0; i < 20; i++) {
+        move_star(stars[i], 3);
+        if (stars[i]->x > 240 || stars[i]->y > 160 || stars[i]->x < 0 || stars[i]->y < 0) {
+            free(stars[i]);
+            stars[i] = create_star();
+        }
+        display_star(stars[i]);
+    }
+
     // Print some debug information
-    int order, row, tick;
-    gbt_get_position(&order, &row, &tick);
-    iprintf("(%s) %d %2d %d | (%s) %d %2d %d\n",
-            gbt_is_playing() ? "ON" : "OFF",
-            order, row, tick,
-            mmActive() ? "ON" : "OFF",
-            mmGetPosition(), mmGetPositionRow(), mmGetPositionTick());
+    // int order, row, tick;
+    // gbt_get_position(&order, &row, &tick);
+    // iprintf("(%s) %d %2d %d | (%s) %d %2d %d\n",
+    //         gbt_is_playing() ? "ON" : "OFF",
+    //         order, row, tick,
+    //         mmActive() ? "ON" : "OFF",
+    //         mmGetPosition(), mmGetPositionRow(), mmGetPositionTick());
+}
+
+s32 RAND(s32 Value)
+{
+   RAND_RandomData *= 20077;
+   RAND_RandomData += 12345;
+
+   return abs(((((RAND_RandomData >> 16) & RAND_MAX) * Value) >> 15)-1);
 }
 
 int main(int argc, char *argv[])
 {
+    RAND_RandomData = (int)VCOUNT;
     irqInit();
-
-    consoleDemoInit();
 
     irqSet(IRQ_VBLANK, vbl_handler);
     irqEnable(IRQ_VBLANK);
 
     // Initialize maxmod with soundbank and 4 channels
-    mmInitDefault((mm_addr)soundbank_bin, 4);
+    // mmInitDefault((mm_addr)soundbank_bin, 4);
     // PSG channels have 1/4th of the range of DMA channels
-    mmSetModuleVolume(1024 / 4);
+    // mmSetModuleVolume(1024 / 4);
+
+    // Draw some dots on the screen
+    REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
+
+    int i;
+    for (i = 0; i < 20; i++) {
+        stars[i] = create_star();
+    }
 
     // Start both songs
-    gbt_play(starlight_brigade_psg, -1);
-    gbt_loop(1);
-    mmStart(MOD_STARLIGHT_BRIGADE_DMA, MM_PLAY_LOOP);
+    // gbt_play(starlight_brigade_psg, -1);
+    // gbt_loop(1);
+    // mmStart(MOD_STARLIGHT_BRIGADE_DMA, MM_PLAY_LOOP);
 
-    while (1)
+    while (1) {
         VBlankIntrWait();
+    }
 }
---
+++ src/toolbox.h
@@ -0,0 +1,61 @@
+// toolbox.h: 
+//
+// === NOTES ===
+// * This is a _small_ set of typedefs, #defines and inlines that can 
+//   be found in libtonc, and might not represent the 
+//   final forms.
+
+
+#ifndef TOOLBOX_H
+#define TOOLBOX_H
+
+typedef u16 COLOR;
+
+#define INLINE static inline
+
+// === (from tonc_memmap.h) ===========================================
+
+#define MEM_IO      0x04000000
+#define MEM_VRAM    0x06000000
+
+// === (from tonc_memdef.h) ===========================================
+
+// --- REG_DISPCNT defines ---
+#define DCNT_MODE0     0x0000
+#define DCNT_MODE1      0x0001
+#define DCNT_MODE2      0x0002
+#define DCNT_MODE3      0x0003
+#define DCNT_MODE4      0x0004
+#define DCNT_MODE5      0x0005
+// layers
+#define DCNT_BG0        0x0100
+#define DCNT_BG1        0x0200
+#define DCNT_BG2        0x0400
+#define DCNT_BG3        0x0800
+#define DCNT_OBJ        0x1000
+
+
+// === (from tonc_video.h) ============================================
+
+#define SCREEN_WIDTH   240
+#define SCREEN_HEIGHT  160
+
+#define vid_mem     ((u16*)MEM_VRAM)
+
+INLINE void m3_plot(int x, int y, COLOR clr)
+{   vid_mem[y*SCREEN_WIDTH+x]= clr;    }
+
+#define CLR_BLACK   0x0000
+#define CLR_RED     0x001F
+#define CLR_LIME    0x03E0
+#define CLR_YELLOW  0x03FF
+#define CLR_BLUE    0x7C00
+#define CLR_MAG     0x7C1F
+#define CLR_CYAN    0x7FE0
+#define CLR_WHITE   0x7FFF
+
+
+INLINE COLOR RGB15(u32 red, u32 green, u32 blue)
+{   return red | (green<<5) | (blue<<10);   }
+
+#endif // TOOLBOX_H
